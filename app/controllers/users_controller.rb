cbbbc824c0595826e7eae5849165d8183ee587b0
Yasuaki Iwata <yasuaki.iwata0101@gmail.com>
2020-03-03 07:51:28 +0900

    ユーザ登録画面追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,30 @@
 class UsersController < ApplicationController
-  def new
+  before_action :set_pref_collection
+
+  def show
+    @user = User.find(params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :prefecture_id, :password, :password_confirmation)
+    end
+
+    def set_pref_collection
+      @pref = Prefecture.all.order(:code)
+    end
 end
+
