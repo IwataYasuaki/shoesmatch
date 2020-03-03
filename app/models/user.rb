861cf4643e044eb849bf1089f5bade12203ae5d7
Yasuaki Iwata <yasuaki.iwata0101@gmail.com>
2020-03-04 00:15:52 +0900

    custom.scss追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,5 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, length: { minimum: 6 }
+  validates :prefecture_id, presence: true
 end
